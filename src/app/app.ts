811b3a1d43982cf05026b7242e3a3cf65710f0e8
Ercan Türkyılmaz <ercanturkyilmaz28@gmail.com>
2026-08-31 03:06:58 +0300

    -Added News and Projects tabs but not sure how to fill

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,12 @@ import { Component, signal } from '@angular/core';
 import { TopBar } from './top-bar/top-bar';
 import { Socials } from './socials/socials';
 import { About } from './about/about';
+import { News } from './news/news';
+import { Projects } from './projects/projects';
 
 @Component({
   selector: 'app-root',
-  imports: [TopBar, Socials, About],
+  imports: [TopBar, Socials, About, News, Projects],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/news/news.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'news',
+  templateUrl: './news.html',
+  styleUrl: './news.css'
+})
+
+export class News {}
